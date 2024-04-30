2da42d03cece3cd53c956f94fc38dcceefeb3ce7
Livia FM <annaliviafmachado@gmail.com>
2024-04-30 02:15:18 -0300

    Create OperadoresAritmeticos

---
+++ src/Codigos/OperadoresAritmeticos/operadoresaritmeticos.kt
@@ -0,0 +1,23 @@
+//Operadores Aritméticos
+/* soma: + 
+* subtração: -
+* divisão: /
+* multiplicação: *
+* resto da divisão: % 
+* incremento: ++
+* decremento: --
+*/ 
+
+fun main( ){
+    var num1 = 20
+    var num2 = 10
+
+    println(num1 + num2)
+    println(num1 - num2)
+    println(num1 / num2)
+    println(num1 * num2)
+    println(num1 % num2)
+    //o ++ ou -- vem ANTES da variável ou número
+    println(++num1)
+    println(--num2)
+}
