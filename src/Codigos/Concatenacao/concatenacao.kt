91afb1c321000a35e850141b0b9000e638eaff65
Livia FM <annaliviafmachado@gmail.com>
2024-04-30 01:58:04 -0300

    Create Concatenacao

---
+++ src/Codigos/Concatenacao/concatenacao.kt
@@ -0,0 +1,16 @@
+//Concatenação de Strings
+
+fun main( ){
+    println("Meu nome eh Anna Livia, mas pode me chamar de Livia! Tenho 21 anos e sou estudante de Engenharia da Computacao")
+    //Se definirmos um texto assim, ele é um texto estático. Logo, não podemos alterá-lo manualmente ;(
+
+    //Com concatenação, podemos completar frases com variáveis!
+
+    //Vamos usar variáveis mutáveis hehe
+    var nome = "Livia"
+    var idade = 21
+    var profissao = "Engenheira da Computacao"
+
+    println("Meu nome eh $nome, tenho $idade anos e sou $profissao")
+    //Muito menos texto do que em java para concatenação
+}
