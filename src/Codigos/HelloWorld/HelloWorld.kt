7260d8726dced3d8b67304567ca240063a60eb6b
Livia FM <annaliviafmachado@gmail.com>
2024-04-30 01:58:58 -0300

    Update HelloWorld

--- src/Codigos/HelloWorld/HelloWorld.kt
+++ src/Codigos/HelloWorld/HelloWorld.kt
@@ -1,7 +1,7 @@
 //Reza a lenda de que se não rodar “Hello, World!” primeiro, não vamos aprender bem a linguagem
 //Quem sou eu pra arriscar?
 
-fun main(){
+fun main( ){
     println("Hello World!")
     println("Um lindo dia para aprender uma nova linguagem!")
 }
