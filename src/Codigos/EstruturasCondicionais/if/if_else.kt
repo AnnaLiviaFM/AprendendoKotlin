ec07f17a920c39b97be94657828711712764a80c
Livia FM <annaliviafmachado@gmail.com>
2024-05-01 17:05:56 -0300

    Update EstruturasCondicionais

--- src/Codigos/EstruturasCondicionais/if/if_else.kt
+++ src/Codigos/EstruturasCondicionais/if/if_else.kt
@@ -1,9 +1,22 @@
 //Estrutura condicional if/else
+// se e senão
 
 fun main( ){
-    if(true){
+/*     if(true){
         println("Verdadeiro")
     }else{
         println("Falso")
     }
+*/
+    var idade = 8
+
+    if(idade <= 10){
+        println("O usuario eh crianca")
+    }else if(idade <= 17){
+        println("O usuario eh adolescente")
+    }else{
+        println("O usuario eh adulto")
+    }
+
+    
 }
