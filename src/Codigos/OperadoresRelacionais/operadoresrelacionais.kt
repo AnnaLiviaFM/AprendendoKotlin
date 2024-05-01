c254c2e85a7b3f3aedc469a115f0b4e590f406dd
Livia FM <annaliviafmachado@gmail.com>
2024-05-01 16:56:04 -0300

    Update OperadoresAritmeticos

--- src/Codigos/OperadoresRelacionais/operadoresrelacionais.kt
+++ src/Codigos/OperadoresRelacionais/operadoresrelacionais.kt
@@ -9,5 +9,18 @@
 */
 
 fun main( ){
-    
+    var valor1 = 10
+    var valor2 = 20
+
+    println(valor1 == valor2)
+    //resposta false
+
+    println(valor1 > valor2)
+    //false
+
+    println(valor1 < valor2)
+    //true
+
+    println(valor1 != valor2)
+    //true
 }
